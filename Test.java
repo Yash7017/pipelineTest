8ec8701f98faf6b51ea55e6730cceeeac6624599
Yash Garg <109918499+Yash7017@users.noreply.github.com>
2024-12-19 00:57:04 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 
 public class Test{
     public static void main(String[] args){
-        System.out.println("this is java program");
+        System.out.println("this is java program,test1");
         
     }
 }
